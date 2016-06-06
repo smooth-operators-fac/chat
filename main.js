48c21d20e417108aba4096a0f167af70bc46ddb7
samgalson@gmail.com <samgalson@gmail.com>
2016-06-06 15:53:10 +0100

    added proper id for output

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-var submit  = document.getElementById('id').addEventListener('click', function(e){
+var submit  = document.getElementById('output').addEventListener('click', function(e){
 
 var inputText = document.getElementById('input-text').value;
 var rude = document.getElementById('rude').checked;
@@ -22,16 +22,16 @@ function response(personality, input){
 if(personality === 'rude'){
 var randomResponse = Math.floor(Math.random()*10);
   if(randomResponse < 3){
-  var b = /\w+/.exec(input);
-  console.log('I hate people who start sentences with the word ' + b);
+   var b = /\w+/.exec(input);
+   output = 'I hate people who start sentences with the word ' + b;
   }
   else if(randomResponse <6){
-  console.log('I dont care what you say, you are a moron');
+   output = 'I dont care what you say, you are a moron';
   }
   else{
 
   }
-
+	return output;
 }
 
 }
