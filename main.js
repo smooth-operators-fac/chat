915369ed50ba85c5ea758aa6d086a6fb6233834e
samgalson@gmail.com <samgalson@gmail.com>
2016-06-06 15:09:05 +0100

    added declaration of output

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var submit  = document.getElementById('id').addEventListener('click', function(e
 
 var inputText = document.getElementById('input-text').value;
 var rude = document.getElementById('rude').checked;
+var output = ''
 
 if (rude){
   output = response('rude', inputText);
@@ -11,7 +12,7 @@ if (rude){
 }
 
 var outputP = document.getElementById('output').appendChild("P");
-var t = document.createTextNode("This is a paragraph.");
+var t = document.createTextNode(output);
 outputP.appendChild(t);
 
 });
