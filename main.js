6df5fc55f2d3e02ee2611873fd632f4a67470b0a
Jakub Svihla <jsvihla@gmail.com>
2016-06-06 15:15:00 +0100

    just a prototype

--- main.js
+++ main.js
@@ -16,3 +16,22 @@ var t = document.createTextNode(output);
 outputP.appendChild(t);
 
 });
+
+function response(personality, input){
+
+if(personality === 'rude'){
+var randomResponse = Math.floor(Math.random()*10);
+  if(randomResponse < 3){
+  var b = /\w+/.exec(input);
+  console.log('I hate people who start sentences with the word ' + b);
+  }
+  else if(randomResponse <6){
+  console.log('I dont care what you say, you are a moron');
+  }
+  else{
+
+  }
+
+}
+
+}
